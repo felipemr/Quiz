c004128ca0fb0193d5dd7909bb25d459b0ed6252
felipemr <felipemr94@outlook.com>
2015-02-23 17:48:22 -0300

    pronto prata+bronze+ouro

--- Quiz/ViewController.h
+++ Quiz/ViewController.h
@@ -10,6 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *pergunta;
+@property (weak, nonatomic) IBOutlet UILabel *resposta;
+@property (weak, nonatomic) IBOutlet UIButton *botaoResposta;
+@property (weak, nonatomic) IBOutlet UIButton *botaoPergunta;
 
+@property (nonatomic) IBOutlet UIImageView *image;
+@property UIImage* imagem0;
+
+@property NSArray *perguntas,*respostas;
+@property UIColor *corBotao,*corFundo;
+
+@property int indice;
+
+- (IBAction)mostrarPergunta:(id)sender;
+- (IBAction)mostrarResposta:(id)sender;
 @end
 
--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -14,9 +14,33 @@
 
 @implementation ViewController
 
+@synthesize perguntas,respostas;
+
+
+-(void)viewWillAppear:(BOOL)animated{
+    _corBotao= [[UIColor alloc]initWithRed: .7 green:.0 blue:.3 alpha:1];
+    _corFundo= [[UIColor alloc]initWithRed: .5 green:.5 blue:1. alpha:1];
+    
+    
+    
+    [_botaoPergunta setBackgroundColor:_corBotao];
+    [_botaoResposta setBackgroundColor:_corBotao];
+    [self.view setBackgroundColor:_corFundo];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _image.image=[UIImage imageNamed:@"img0.png"];
+    
+    
+    perguntas = [[NSArray alloc]initWithObjects:(NSString *)@"Pergunta",@"7+7=?", @"Qual o meu nome?",@"qual a raiz de -1?", nil];
+    
+    [_pergunta setText:perguntas[0]];
+    _indice=1;
+    
+    respostas = [[NSArray alloc]initWithObjects:(NSString *)@"?????",@"14", @"Felipe",@"nao existe raiz de numeros negativos", nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +48,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)mostrarPergunta:(id)sender {
+    [_pergunta setText: perguntas[_indice]];
+    _image.image=[UIImage imageNamed:@"img0.png"];
+    [_resposta setText: respostas[0]];
+    
+    _indice++;
+    
+    if(_indice>[perguntas count]){
+        _indice=0;
+    }
+    
+    
+}
+
+- (IBAction)mostrarResposta:(id)sender {
+    [_resposta setText: respostas[_indice-1]];
+    _image.image=[UIImage imageNamed:[[@"img" stringByAppendingString:[NSString stringWithFormat:@"%i",_indice-1]] stringByAppendingString:@".png"] ];
+    
+    if(_indice>=[respostas count]){
+        _indice=0;
+    }
+}
 @end
