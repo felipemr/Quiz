580f5b194fbd03cd2de759a148de039216567540
felipemr <felipemr94@outlook.com>
2015-02-23 14:59:49 -0300

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Felipe Marques Ramos on 23/02/15.
+//  Copyright (c) 2015 Felipe Marques Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
